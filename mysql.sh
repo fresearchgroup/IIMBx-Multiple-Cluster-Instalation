70745e97a5c1c0acc8faa23a09afa976434c9174
abhisgithub <abhijitbonik@hotmail.com>
2017-09-08 04:19:12 +0530

    mysql dump issue solved

--- mysql.sh
+++ mysql.sh
@@ -9,10 +9,11 @@ sudo -S <<< $1 debconf-set-selections <<< "mysql-server mysql-server/root_passwo
 sudo -S <<< $1 apt-get install -y mysql-server
 
 echo "Downloaing IIMbx database from Git Lab"
-wget http://gitlab.cse.iitb.ac.in/mangeshg/iimbx/blob/589dd9a9511e32545d9b9c65d3a77498c89de7fd/iimbx_21082017.sql
+wget http://gitlab.cse.iitb.ac.in/mangeshg/iimbx/raw/master/iimbx_21082017.sql
 
 echo "Creating a iimbx database"
 mysql -uroot -proot -e "CREATE DATABASE iimbx ;"
 
 echo "Importing the database on Mysql server"
-mysql -u root -proot iimbx < iimbx_*.sql
+mysql -u root -proot iimbx < iimbx_21082017.sql
+
