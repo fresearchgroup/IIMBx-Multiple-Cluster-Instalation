6fc79f04b70607dd2ea333fe350fd9489d62c5b3
firuza <firya87@gmail.com>
2017-09-08 18:51:40 +0530

    Reading ip, name, password of 4VMs from user

--- cluster_install.sh
+++ cluster_install.sh
@@ -1,51 +1,77 @@
 #provide the nfs ip adress. It will be on of the two ip addresses of Web servers where drupal will be installed
-web1=10.129.26.25
-web2=10.129.26.77
 
-#assuming that all machine have same user name .eg:- edx 
+echo "Enter password for your current machine"
+read -s currsyspassword
 
-#assuming that all machines have same password 
-password=123456
+echo "Enter details of the machine for MySQL installation"
+echo "Enter IP address: "
+read mysqlipaddress
+echo "Enter username: "
+read mysqlusername
+echo "Enter password: "
+read -s mysqlpassword
 
+echo "Enter details of the machine for Web 1"
+echo "Enter IP address: "
+read web1ipaddress
+echo "Enter username: "
+read web1username
+echo "Enter password: "
+read -s web1password
+
+echo "Enter details of the machine for Web 2"
+echo "Enter IP address: "
+read web2ipaddress
+echo "Enter username: "
+read web2username
+echo "Enter password: "
+read -s web2password
+
+echo "Enter details of the machine for HAProxy"
+echo "Enter IP address: "
+read haproxyipaddress
+echo "Enter username: "
+read haproxyusername
+echo "Enter password: "
+read -s haproxypassword
 
 #!/bin/bash
 #install sshpass for accessing remote by providing password as an argument
-sudo -S <<< "$password" apt-get install sshpass
+sudo -S <<< "$currsyspassword" apt-get install sshpass
 
-#Provide the ip address of Mysql 
 #Copy the script to be executed on mysql server
-sshpass -p "$password" scp mysql.sh edx@10.129.46.102:~/ 
+sshpass -p "$mysqlpassword" scp mysql.sh $mysqlusername@$mysqlipaddress:~/ 
 
 #run the script  on mysql server.
-sshpass -p "$password" ssh edx@10.129.46.102 /home/edx/mysql.sh $password
+sshpass -p "$mysqlpassword" ssh $mysqlusername@$mysqlipaddress /home/edx/mysql.sh $mysqlpassword
 
 #Copy the script to web 1
-sshpass -p "$password" scp web.sh edx@10.129.26.25:~/ 
+sshpass -p "$web1password" scp web.sh $web1username@$web1ipaddress:~/ 
 
 #Execute the script on web 1
-sshpass -p "$password" ssh edx@10.129.26.25 /home/edx/web.sh $password
+sshpass -p "$web1password" ssh $web1username@$web1ipaddress /home/edx/web.sh $web1password
 
 #Copy the script to web 2
-sshpass -p "$password" scp web.sh edx@10.129.26.77:~/ 
+sshpass -p "$web2password" scp web.sh $web2username@$web2ipaddress:~/ 
 
 #Execute the script on web 2
-sshpass -p "$password" ssh edx@10.129.26.77 /home/edx/web.sh $password
+sshpass -p "$web2password" ssh $web2username@$web2ipaddress /home/edx/web.sh $web2password
 
 #Copy the script to haproxy server
-sshpass -p "$password" scp haproxy.sh edx@10.129.28.120:~/ 
+sshpass -p "$haproxypassword" scp haproxy.sh $haproxyusername@haproxyipaddress:~/ 
 
 #Execute the script on haproxy server
-sshpass -p "$password" ssh edx@10.129.28.120 /home/edx/haproxy.sh $password $web1 $web2
+sshpass -p "$haproxypassword" ssh $haproxyusername@haproxyipaddress /home/edx/haproxy.sh $haproxypassword $web1ipaddress $web2ipaddress
 
 
 #Copy the script to nfs server
-sshpass -p "$password" scp nfs_server.sh edx@10.129.26.77:~/ 
+sshpass -p "$web2password" scp nfs_server.sh $web2username@$web2ipaddress:~/ 
 
 #Execute the script in nfs server 
-sshpass -p "$password" ssh edx@10.129.26.77 /home/edx/nfs_server.sh $password $web2
+sshpass -p "$web2password" ssh $web2username@$web2ipaddress /home/edx/nfs_server.sh $web2password $web2ipaddress
 
 #Copy the script to nfs client
-sshpass -p "$password" scp nfs_client.sh edx@10.129.26.25:~/ 
+sshpass -p "$web1password" scp nfs_client.sh $web1username@$web1ipaddress:~/ 
 
 #Execute the script on nfs client 
-sshpass -p "$password" ssh edx@10.129.26.25 /home/edx/nfs_client.sh $password $web1
+sshpass -p "$web1password" ssh $web1username@$web1ipaddress /home/edx/nfs_client.sh $web1password $web1ipaddress
